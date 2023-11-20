1de4a6db571d8d9156e5a2d9b1bd60c1f733bb07
Jeanpierre Ponte Solis <jponte@tectronix.cl>
2023-11-20 02:11:24 -0500

    RE: Fix UTC

--- controllers/api/notification.js
+++ controllers/api/notification.js
@@ -6,9 +6,6 @@ var admin = require("firebase-admin");
 notificationRouter.post("/scheduleCreate", (req, res) => {
   const { nombre, descripcion, fechaHora, token } = req.body;
   let dateConverter = new Date(fechaHora);
-  const limaOffset = -5 * 60; // -5 horas en minutos
-  dateConverter.setMinutes(dateConverter.getMinutes() + dateConverter.getTimezoneOffset() - limaOffset);
-  console.log(dateConverter);
   var j = schedule.scheduleJob(dateConverter, function () {
     var message = {
       notification: {
